c4edaedd93dc9e36fba70ccc46d8ab7f693f569f
loupancas <129621626+loupancas@users.noreply.github.com>
2024-08-30 16:42:38 -0500

    escenas

---
+++ Assets/Scripts/ScenesChanger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesChanger : MonoBehaviour
+{
+    public int sceneIdToLoad;  
+
+    private void OnCollisionEnter(Collision collision)
+    {
+      
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    
+
+   
+
+}
