b602d8e3cad35c8602cecd39d1ad7cc23ce575f4
escler <fanelli.leandro.sebastian@gmail.com>
2024-09-10 18:09:02 -0300

    Changes house

--- Assets/Scripts/New/Items/Cross.cs
+++ Assets/Scripts/New/Items/Cross.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cross : Item
 {
+    private float _currentTime;
+    public float neededTime, coolDown;
+    public override void OnInteract(bool hit, RaycastHit i)
+    {
+        base.OnInteract(hit, i);
+        if (Input.GetButton("Interact"))
+        {
+            _currentTime += Time.deltaTime;
+            if (_currentTime >= neededTime)
+            {
+                print("Use la cruz");
+                _currentTime = 0;
+            }
+        }
+
+        if (Input.GetButtonUp("Interact")) _currentTime = 0;
+    }
 
 }
