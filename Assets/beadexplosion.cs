e57f1be2baede0c3440d18b2a57555643bb98d78
Arfilion <105883987+Arfilion@users.noreply.github.com>
2025-04-16 02:34:47 -0300

    rosary

---
+++ Assets/beadexplosion.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class beadexplosion : MonoBehaviour
+{
+    public Animator beadAnimator;
+    [SerializeField] List<VisualEffect> particles = new List<VisualEffect>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        beadAnimator = GetComponent<Animator>();
+        beadAnimator.SetBool("used", false);
+        foreach (VisualEffect item in particles)
+        {
+            item.Stop();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            beadAnimator.SetBool("used", true);
+            foreach (VisualEffect item in particles)
+            {
+                item.Play();
+            }
+
+        }
+
+    }
+}
