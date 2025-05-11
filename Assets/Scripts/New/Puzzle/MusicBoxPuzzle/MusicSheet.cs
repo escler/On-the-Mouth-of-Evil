eef8eb8e53ed917b88aae87e21889e1708fa24f2
escler <fanelli.leandro.sebastian@gmail.com>
2025-05-11 14:12:56 -0300

    Music sheet with inspect item

--- Assets/Scripts/New/Puzzle/MusicBoxPuzzle/MusicSheet.cs
+++ Assets/Scripts/New/Puzzle/MusicBoxPuzzle/MusicSheet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,25 @@ public class MusicSheet : Item
 {
     [SerializeField] private int number;
     public int Number => number;
+    
+    public Transform cameraPos, handPos;
+    private bool canInteract, active;
+    public float offset;
+    private Vector3 reference = Vector3.zero;
+    private float _sensX, _sensY;
+    private PlayerCam _playerCam;
+    [SerializeField] private string tag;
+
+
+    private void Start()
+    {
+        active = false;
+        cameraPos = PlayerHandler.Instance.cameraPos;
+        handPos = PlayerHandler.Instance.handPivot;
+        _sensX = PlayerHandler.Instance.playerCam.sensX;
+        _sensY = PlayerHandler.Instance.playerCam.sensY;
+        _playerCam = PlayerHandler.Instance.playerCam;
+    }
 
     public override void OnGrabItem()
     {
@@ -23,6 +43,7 @@ public class MusicSheet : Item
     {
         if (!hit) return;
         if (!canInteractWithItem) return;
+        if (active) return;
         
         if (i.transform.TryGetComponent(out SheetSlot sheetSlot))
         {
@@ -42,6 +63,7 @@ public class MusicSheet : Item
         {
             OnInteract(rayConnected, ray);
         }
+        if(Input.GetButtonDown("Focus"))FocusObject();
     }
     
     public override bool CanInteractWithItem()
@@ -74,5 +96,69 @@ public class MusicSheet : Item
             timer += Time.deltaTime * 0.5f;
             yield return null;
         }
+    }
+    
+    public override void FocusObject()
+    {
+        if (canInteract) return;
+        active = !active;
+        CanvasManager.Instance.rotateInfo.SetActive(active);
+        Inventory.Instance.cantSwitch = active;
+        StartCoroutine(active ? FocusObjectCor() : UnFocusObject());
+        transform.localScale = Vector3.one;
+    }
+    
+    IEnumerator FocusObjectCor()
+    {
+        cantBobbing = true;
+        canInteract = true;
+        transform.SetParent(null);
+        PlayerHandler.Instance.UnPossesPlayer();
+        while (Vector3.Distance(transform.position, cameraPos.position + cameraPos.forward * offset) > 0.1f)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, cameraPos.position + cameraPos.forward * offset, ref reference, .1f);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        transform.position = cameraPos.position + cameraPos.forward * offset;
+        canInteract = false;
+    }
+
+    IEnumerator UnFocusObject()
+    {
+        cantBobbing = false;
+        canInteract = true;
+        Inventory.Instance.cantSwitch = true;
+        transform.SetParent(handPos);
+        PlayerHandler.Instance.PossesPlayer();
+        while (Vector3.Distance(transform.position, handPos.position) > 0.1f)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, handPos.position, ref reference, .1f);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        transform.position = handPos.position;
+        canInteract = false;
+        Inventory.Instance.cantSwitch = false;
+        cantBobbing = false;
+    }
+
+    private void Update()
+    {
+        if (!active) return;
+        RotateObject();
+        
+    }
+
+    void RotateObject()
+    {
+            
+        float XaxisRotation = Input.GetAxis("Horizontal") * _sensX * Time.deltaTime;
+        float YaxisRotation = Input.GetAxis("Vertical") * _sensY *Time.deltaTime;
+        float ZaxisRotation = Input.GetAxis("ZAxis") * _sensY *Time.deltaTime;
+
+        transform.RotateAround(transform.position, _playerCam.transform.right, YaxisRotation);
+        transform.RotateAround(transform.position, _playerCam.transform.up, XaxisRotation);
+        transform.RotateAround(transform.position, _playerCam.transform.forward, ZaxisRotation);
     }
 }
