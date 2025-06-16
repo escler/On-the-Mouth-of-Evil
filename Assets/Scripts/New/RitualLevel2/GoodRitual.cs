6073a46455be06e42b578480f5ad0686448ba43d
escler <fanelli.leandro.sebastian@gmail.com>
2025-06-16 18:51:25 -0300

    Good Ritual Sequence - Anim - GodRay

--- Assets/Scripts/New/RitualLevel2/GoodRitual.cs
+++ Assets/Scripts/New/RitualLevel2/GoodRitual.cs
@@ -9,6 +9,7 @@ public class GoodRitual : MonoBehaviour
     public bool leverActivated;
     [SerializeField] private Transform ritualPos;
     [SerializeField] private int level;
+    [SerializeField] private GameObject godRays;
 
     private void Awake()
     {
@@ -29,6 +30,8 @@ public class GoodRitual : MonoBehaviour
 
     IEnumerator GoodRitualSteps()
     {
+        var enemy = MorgueEnemy.Instance;
+        enemy.ritualDone = true;
         PlayerHandler.Instance.movement.ritualCinematic = true;
         Inventory.Instance.cantSwitch = true;
 
@@ -36,7 +39,44 @@ public class GoodRitual : MonoBehaviour
         
         //Lo que activamos al hacer el good ritual
 
+        yield return new WaitUntil(() => MorgueEnemy.Instance.inRitualNode);
+        while (enemy.enemyVisibility < 4)
+        {
+            enemy.enemyVisibility += Time.deltaTime * 1.6f;
+            enemy.enemyVisibility = Mathf.Clamp(enemy.enemyVisibility, 0, 8);
+            enemy.enemyMaterial.SetFloat("_Power", enemy.enemyVisibility);
+            print(enemy.enemyVisibility);
+
+            if (enemy.enemyVisibility >= 2f)
+            {
+                //_enemyAnimator.ChangeStateAnimation("Absorb", true);
+            }
+
+            yield return null;
+        }
+        print("Bad Ritual Sali del While Aparecer");
+        
+        enemy.anim.ChangeState("Exorcism", true);
+        
+        yield return new WaitUntil(() => enemy.anim.animator.GetCurrentAnimatorStateInfo(0).IsName("Burn"));
+        var enemyClip = enemy.anim.animator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
+        var speed = enemy.anim.animator.speed;
+        var realDuration = enemyClip / speed;
+        float startVisibility = enemy.enemyVisibility;
+        float elapsed = 0f;
+
+        while (elapsed < realDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / realDuration);
+            enemy.enemyVisibility = Mathf.Lerp(startVisibility, 0f, t);
+            enemy.enemyMaterial.SetFloat("_Power", enemy.enemyVisibility);
+
+            yield return null;
+        }
+        
         yield return new WaitForSeconds(1f);
+        godRays.SetActive(true);
 
         RitualManager.Instance.levitatingItems[0].SetActive(true);
         RitualManager.Instance.actualItemActive = RitualManager.Instance.levitatingItems[0].gameObject;
